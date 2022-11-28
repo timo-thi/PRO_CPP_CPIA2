6f8bab2545394b2c8e5420f369dae93da8b1d53d
Leandro De Barros Barbosa <115976416+Pandax33@users.noreply.github.com>
2022-11-28 13:50:56 +0100

    header remplie

--- PRO_CPP_CPIA2/CM_Connexion.cpp
+++ PRO_CPP_CPIA2/CM_Connexion.cpp
@@ -1,1 +1,48 @@
 #include "CM_Connexion.h"
+CM_Connexion::CM_Connexion() {
+	this->requete = "RIEN";
+	this->connexion = "Data Source=34.163.225.147;Initial Catalog=leandroBDD;Persist Security Info=True;User ID=sqlserver;Password=Prosit6";
+	this->connecteur = gcnew SqlConnection(this->connexion);
+	this->commande = gcnew SqlCommand(this->requete, this->connecteur);
+	this->commande->CommandType = CommandType::Text;
+}
+
+
+int CM_Connexion::actionRowID(String^ requete) {
+	int id;
+	this->setSQL(requete);
+	this->commande->CommandText = this->requete;
+	this->connecteur->Open();
+	id = Convert::ToInt32(this->commande->ExecuteScalar());
+	this->connecteur->Close();
+	return id;
+}
+void  CM_Connexion::actionRows(String^ requete)
+{
+	this->setSQL(requete);
+	this->commande->CommandText = this->requete;
+	this->connecteur->Open();
+	this->commande->ExecuteNonQuery();
+	this->connecteur->Close();
+}
+DataSet^ CM_Connexion::getRows(String^ requete, String^ Table)
+{
+	this->setSQL(requete);
+	this->adapteur = gcnew SqlDataAdapter(this->commande);
+	this->commande->CommandText = this->requete;
+	this->DS = gcnew DataSet();
+	this->adapteur->Fill(this->DS, Table);
+	return this->DS;
+}
+void CM_Connexion::setSQL(String^ requete)
+{
+	if (requete == "" || requete == "RIEN")
+	{
+		this->requete = "RIEN";
+	}
+	else
+	{
+		this->requete = requete;
+	}
+}
+
--- PRO_CPP_CPIA2/CM_Connexion.h
+++ PRO_CPP_CPIA2/CM_Connexion.h
@@ -1,5 +1,23 @@
 #pragma once
+using namespace System::Data;
+using namespace System::Data::SqlClient;
+using namespace System::Xml;
+using namespace System;
 ref class CM_Connexion
 {
+private:
+	String^ connexion;
+	String^ requete;
+	SqlConnection^ connecteur;
+	SqlCommand^ commande;
+	SqlDataAdapter^ adapteur;
+	DataSet^ DS;
+	void setSQL(String^);
+public:
+	CM_Connexion();
+	int actionRowID(String^);
+	void actionRows(String^);
+	DataSet^ getRows(String^, String^);
+
 };
 
