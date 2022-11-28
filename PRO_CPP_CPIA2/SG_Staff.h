097d85dde69b79327c07e94c46f96135e017ce7b
Eralo <115709869+Eralo@users.noreply.github.com>
2022-11-28 12:28:42 +0100

    Header remplis

--- PRO_CPP_CPIA2/SG_Staff.h
+++ PRO_CPP_CPIA2/SG_Staff.h
@@ -2,7 +2,15 @@
 #include "SG_Personne.h"
 
 ref class SG_Staff :
-    public SG_Personne
-{
+    public SG_Personne {
+
+public:
+
+    SG_Staff();
+
+    void Add_Staff();
+    void Remove_Staff();
+    void Update_Staff();
+    void Fetch_Staff();
 };
 
